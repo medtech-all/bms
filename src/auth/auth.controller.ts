6245dc5e09d5dedc25709f688ad87034e4434c28
mohammadtavassolian3 <mohammadtavassolian3@gmail.com>
2023-08-14 01:19:19 +0330

    create user interceptor

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,9 +2,14 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { CreateUserDto } from 'src/common/dto/user/create-user.dto';
 import { UserService } from 'src/common/providers/user/user.service';
 import { HashingService } from './hashing.service';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { JSendTransformInterceptor } from 'src/common/interceptors/JSendTransform.interceptor';
+import { SerializeInterceptor } from 'src/common/interceptors/serializer.interceptor';
+import { UserDto } from 'src/common/dto/user/user.dto';
+import { UseInterceptors } from '@nestjs/common/decorators';
 
 @Controller('auth')
+@ApiTags("Auth")
 export class AuthController {
     constructor(private readonly userService: UserService
         , private readonly hashingService: HashingService
@@ -14,8 +19,10 @@ export class AuthController {
     @ApiOperation({ summary: 'Create a new user' })
     @ApiResponse({ status: 201, description: 'User created successfully' })
     @ApiResponse({ status: 400, description: 'Bad Request' })
+    @UseInterceptors(new SerializeInterceptor(UserDto))
+    @UseInterceptors(JSendTransformInterceptor)
     async register(@Body() createUserDto: CreateUserDto) {
         let hashedPassword = await this.hashingService.hashPassword(createUserDto.password, 10)
-        return await this.userService.create({ ...createUserDto, password: hashedPassword })
+        return { data: await this.userService.create({ ...createUserDto, password: hashedPassword }), status: "success", message: "Inserted" }
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from "@nestjs/common"
-import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
-  app.useGlobalFilters(new HttpExceptionFilter());
   const config = new DocumentBuilder()
     .setTitle('Noora Building Management System')
     .setDescription('API documents')
